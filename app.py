5a7f90000e585d1f87ca87da9d3a3685177f09f8
Hiba Dawood <164556977+HibaDawood@users.noreply.github.com>
2025-02-21 20:47:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ if uploaded_files:
             if file_ext == ".csv":
                 df = pd.read_csv(file)
             elif file_ext == ".xlsx":
-                df = pd.read_excel(file, engine="openxlsx")
+                df = pd.read_excel(file, engine="openpyxl")
             else:
                 st.error(f"Unsupported file type: {file_ext}")
                 continue
